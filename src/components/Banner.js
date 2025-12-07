a545282f958696eb59881c456a0919b14f916538
Ankita Sood <ankitasood2000@gmail.com>
2025-12-07 13:37:29 +0530

    Changed css for banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,13 @@
 const Banner = () => {
   return (
-    <section className="flex flex-col md:flex-row items-center justify-between px-10 md:px-20 py-20 gap-10 animate-fadeIn">
-      <div className="flex flex-col flex-1 justify-center">
+    <section className="flex flex-col md:flex-row items-center justify-center px-6 md:px-10 py-20 gap-6 animate-fadeIn">
+      <div className="flex flex-col flex-1 justify-center max-w-xl">
         <h1 className="text-5xl md:text-6xl font-extrabold m-2 tracking-wide bg-gradient-to-r from-cyan-400 to-blue-600 text-transparent bg-clip-text">
           React Projects
         </h1>
+
         <p className="text-xl m-2 leading-[1.5] tracking-wide">
-          Here are some of my recent projects made with{" "}
+          Here are some of my recent projects made with
           <span className="font-bold italic underline underline-offset-1">
             React
           </span>
@@ -17,12 +18,12 @@ const Banner = () => {
           href="https://www.linkedin.com/in/ankitasood2000"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-[#60DBFB] p-3 max-w-48 rounded-md m-2 font-medium transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+          className="bg-[#60DBFB] p-3 max-w-48 rounded-md m-2 font-medium inline-block transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
         >
           LINKEDIN PROFILE
         </a>
       </div>
-      <div className="flex-1 flex justify-center">
+      <div className="flex-1 flex justify-center max-w-xl">
         <img
           src="/hero-1.7c6a8341.svg"
           alt="Hero Graphic"
